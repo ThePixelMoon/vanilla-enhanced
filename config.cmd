66fba440f93aba10c9c8038e695ad8d44b52e82f
YourLocalMoon <yourlocalmoon7@gmail.com>
2025-05-02 12:03:46 +0300

    try to fix

--- config.cmd
+++ config.cmd
@@ -1,3 +1,3 @@
 @echo off
-cmake -DCMAKE_INSTALL_PREFIX="./out" -G "Visual Studio 17 2022" -A Win32 -B build
+cmake -DCMAKE_INSTALL_PREFIX="./out" -G "Visual Studio 17 2022" -A Win32 -B build --fresh
 pause
